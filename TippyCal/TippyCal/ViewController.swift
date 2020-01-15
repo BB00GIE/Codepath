7b4c0fb13f298f3adffda95ff9e0c997b15d0749
Boogie <bmelmore18@students.desu.edu>
2020-01-15 18:58:32 -0500

    Almost Done

--- TippyCal/TippyCal/ViewController.swift
+++ TippyCal/TippyCal/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func CalcText(_ sender: Any) {
+        //get bill
+        let bill = Double(billField.text!) ?? 0
+        
+        //Calc tip and total
+        let tip = bill * 0.1
+        let total = bill + tip
+        
+        // Update tip and total amount
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+    }
 }
 
